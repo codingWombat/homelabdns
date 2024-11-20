1fd91ac652d6d852ebf3cf8ef25c3a3da3351c5a
Thomas Budde <mail@codingwombat.dev>
2024-11-20 19:26:54 +0100

    changed handling to be in a loop and added stop signal handling

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use reqwest::Error;
 use chrono::{DateTime, Utc};
 use serde_json::json;
 use std::env;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 #[derive(Deserialize, Debug)]
 struct IpResponse {
@@ -65,33 +67,53 @@ impl Configuration {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let configuration = Configuration::load()?;
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
 
-    let dns_record = load_dns_records(&configuration).await?.result;
-    println!("{:?}", dns_record);
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    }).expect("Error setting Ctrl-C handler");
 
+    let mut interval_timer = tokio::time::interval(chrono::Duration::seconds(30)
+        .to_std().unwrap());
+
+    while running.load(Ordering::SeqCst) {
+        let dns_record = load_dns_records(&configuration).await?.result;
+        'inner: loop {
+            interval_timer.tick().await;
+            println!("{:?}", dns_record);
+            if do_stuff(&configuration, &dns_record).await? || !running.load(Ordering::SeqCst) {
+                break 'inner;
+            }
+        }
+    }
+    Ok(())
+}
+
+async fn do_stuff(ref configuration: &Configuration, ref dns_record: &DnsRecord) -> Result<bool, Error> {
     let ip_response = load_external_ip().await?;
+    let mut ip_updated = false;
     println!("{:?}", ip_response.ip);
 
     if ip_response.ip == dns_record.content {
         println!("Up to date!");
     } else {
         println!("IP needs to be updated");
-
         let patch_request = DnsPatchRequest {
-            id: dns_record.id,
+            id: dns_record.id.to_string(),
             comment: Some("Ip updated by homelabdns".to_string()),
-            name: dns_record.name,
+            name: dns_record.name.to_string(),
             proxied: dns_record.proxied,
             ttl: dns_record.ttl,
-            content: dns_record.content,
-            type_field: dns_record.type_field,
+            content: dns_record.content.to_string(),
+            type_field: dns_record.type_field.to_string(),
         };
         update_dns_record(&configuration, patch_request).await?;
-    }
-
-    Ok(())
+        ip_updated = true;
+    };
+    Ok(ip_updated)
 }
 
 async fn load_external_ip() -> Result<IpResponse, Error> {
